94736e4c8bd32e054a52ad849f47c58703eaa5ff
Francesco Luppi <francescoluppi@aol.com>
2020-12-30 23:20:41 +0100

    Adding tailwindCSS to style the all thing up

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
+import './index.css'
 
 var firebaseConfig = {
     apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
