5ae4e864bcd492956c9e30dff785f41ef20d4799
Francesco Luppi <francescoluppi@aol.com>
2020-12-30 23:07:31 +0100

    Deleting the two first page Home and About because empty

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import About from '../views/About.vue'
 import Signup from '../views/Signup.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/login',
     name: 'Login',
     component: Login
   },{
