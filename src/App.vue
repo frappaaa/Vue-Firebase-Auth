70a7b006bae7747ffd287d3f3d18adfbd64d332f
Francesco Luppi <francescoluppi@aol.com>
2020-12-31 01:07:20 +0100

    fix typo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     id="nav"
     class="grid grid-cols-2 gap-4 fixed"
   >
-    <router-link to="/"><button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button></router-link>
+    <router-link to="/login"><button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button></router-link>
     <router-link to="/signup"><button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Signup</button></router-link>
   </div>
   <router-view />
